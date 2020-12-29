7868858ae811700cb9e99e05869d5a52ad34af42
RobinMglsk <robin@migalski.be>
2020-12-29 18:38:27 +0100

    Finish drill 4

--- 4_drill_6.cpp
+++ 4_drill_6.cpp
@@ -1,48 +1,108 @@
 #include "./std_lib_facilities.h"
 
-bool checkIfUnitValid(string unit){
-    vector<string> validUnits {"cm", "m", "in", "ft"};
-    return !(find(validUnits.begin(), validUnits.end(),unit) != validUnits.end());
+constexpr double CM_TO_M{0.01};
+constexpr double IN_TO_M{2.54 * CM_TO_M};
+constexpr double FT_TO_M{12 * IN_TO_M};
+
+double convertToM(double number, string unit)
+{
+
+    double cv{0.0};
+
+    if (unit == "m")
+    {
+        cv = number;
+    }
+    else if (unit == "cm")
+    {
+        cv = number * CM_TO_M;
+    }
+    else if (unit == "in")
+    {
+        cv = number * IN_TO_M;
+    }
+    else if (unit == "ft")
+    {
+        cv = number * FT_TO_M;
+    }
+
+    return cv;
 }
 
-int main(){
+bool checkIfUnitValid(string unit)
+{
+    vector<string> validUnits{"cm", "m", "in", "ft"};
+    return !(find(validUnits.begin(), validUnits.end(), unit) != validUnits.end());
+}
 
-    double number {0};
-    string unit {""};
+double getSum(vector<double> values)
+{
 
-    double smallest {0};
-    double largest {0};
-    int count {0};
+    double sum{0.0};
 
-    while(cin >> number >> unit){
+    for (int i = 0; i < values.size(); i++)
+    {
+        sum += values[i];
+    }
 
-        if(checkIfUnitValid(unit)){
+    return sum;
+}
+
+int main()
+{
+
+    double value{0};
+    string unit{""};
+
+    double smallest{0};
+    double largest{0};
+
+    vector<double> values{};
+
+    while (cin >> value >> unit)
+    {
+
+        if (checkIfUnitValid(unit))
+        {
             cout << "Invalid unit enterd. Please use cm, m, in or ft." << endl;
             continue;
         }
 
-        cout << number;
+        double convertedValue = convertToM(value, unit);
 
-        if(count == 0){
-            smallest = number;
-            largest = number;
+        cout << convertedValue << "m" << endl;
+
+        if (values.size() == 0)
+        {
+            smallest = convertedValue;
+            largest = convertedValue;
         }
 
-        if(number < smallest){
-            smallest = number;
-            cout << " the smallest so far";
+        if (convertedValue < smallest)
+        {
+            smallest = convertedValue;
+            cout << " the smallest so far" << endl;
         }
 
-        if(number > largest){
-            largest = number;
-            cout << " the largest so far";
+        if (convertedValue > largest)
+        {
+            largest = convertedValue;
+            cout << " the largest so far" << endl;
         }
 
-        cout << endl;
-        
-        ++count;
+        values.push_back(convertedValue);
+    }
+
+    cout << "Smallest value is: " << smallest << "m" << endl;
+    cout << "Largest value is: " << largest << "m" << endl;
+    cout << "Sum of all values is: " << getSum(values) << "m" << endl;
+    cout << "Number of values: " << values.size() << endl;
+
+    sort(values);
+    cout << "Values: ";
+    for(int i = 0; i < values.size(); i++){
+        cout << values[i] << "m ";
     }
 
     return 0;
-
 }
